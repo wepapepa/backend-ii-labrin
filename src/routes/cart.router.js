5c8e0932c65b11a13f492d9d41b8013b12fb1504
Pepa Labrin <catu.leibrin@gmail.com>
2024-09-28 14:03:19 -0300

    added missing dependencias

---
+++ src/routes/cart.router.js
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import CartController from 'c../controllers/cart.controllers.js';
+import { checkAdmin } from "../middlewares/checkAdmin.js";
+import { passportCall } from "../middlewares/passportCall.js";
+const controller = new CartController();
+
+const router = Router();
+
+router.get("/", [passportCall('current'), checkAdmin], controller.getAll);
+router.get("/:id", [passportCall('current')], controller.getById);
+router.post("/", [passportCall('current'), checkAdmin], controller.create);
+router.put("/:id", [passportCall('current'), checkAdmin], controller.update);
+router.delete("/:id", [passportCall('current'), checkAdmin], controller.remove);
+
+router.post("/:idCart/products/:idProd", [passportCall('current')], controller.addProdToCart);
+router.delete("/:idCart/products/:idProd",[passportCall('current')], controller.removeProdToCart);
+router.put("/:idCart/products/:idProd",[passportCall('current')], controller.updateProdQuantityToCart);
+router.delete("/clear/:idCart", [passportCall('current')], controller.clearCart);
+
+export default router;
---
+++ src/routes/product.router.js
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import ProductController from "../controllers/product.controllers.js";
+import { checkAdmin } from "../middlewares/checkAdmin.js";
+import { passportCall } from "../passpprt/passportCall.js";
+
+const controller = new ProductController()
+
+const router = Router();
+
+router.get('/', [passportCall('current')], controller.getAll);
+router.get('/:id', [passportCall('current')], controller.getById);
+
+Router.post('/', [passportCall('current'), checkAdmin], controller.create);
+Router.put('/:id', [passportCall('current'), checkAdmin], controller.update);
+Router.delete('/:id', [passportCall('current'), checkAdmin], controller.remove);
+
+export default router;
