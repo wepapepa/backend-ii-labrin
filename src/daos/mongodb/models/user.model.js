f6a93a9fd7ac4d82096828d54f5af19fd52ea6b0
Pepa Labrin <catu.leibrin@gmail.com>
2024-09-29 01:38:44 -0300

    controllers and models

---
+++ src/daos/mongodb/models/user.model.js
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+
+const usersSchema = new Schema({
+    first_name: {
+        type: String,
+        required: true,
+    },
+    last_name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    
+})
