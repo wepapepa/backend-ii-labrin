d7e7a427f6994472afeb0b736e3e9bdccab5310f
Pepa Labrin <catu.leibrin@gmail.com>
2024-10-04 22:30:14 -0300

    conecté manualmente la mongodb

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,12 +1,18 @@
 import { connect } from 'mongoose';
 import 'dotenv/config';
+import { configDotenv } from 'dotenv';
+
+configDotenv();
 
 export const initMongoDB = async() => {
+    console.log('MONGO_ATLAS_URL:', process.env.MONGO_ATLAS_URL);
+    console.log('MONGO_LOCAL_URL:', process.env.MONGO_LOCAL_URL);
 
     try {
-        await connect(process.env.MONGO_ATLAS_URL || process.env.MONGO_LOCAL_URL);
+        await connect('mongodb+srv://admincoder:oCBds46JdGGWjRrZ@cluster0.crueo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('conectado a la base de datos de MongoDB :)');
     } catch (error) {
+        console.error('Error de conexión a MongoDB:c', error);
         throw new Error(error);
     }
 };
